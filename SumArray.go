d1a1764184f0895171d86694972572c3f68aa6c6
aravindhshankar <aravindhswaminathan@gmail.com>
2026-01-03 00:13:12 +0100

    init

---
+++ SumArray.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sum(ch chan int, arr []int) {
+	total := 0
+	for _, val := range arr {
+		total = total + val
+	}
+	ch <- total
+}
+
+func main() {
+	var total int
+	c := make(chan int, 2)
+	N := 10
+	list := make([]int, N)
+	for i := 0; i < N; i++ {
+		list[i] = i + 1 // Numbers from 1 to N
+	}
+
+	go sum(c, list[0:N/2])
+	go sum(c, list[N/2:N])
+	total = <-c
+	total += <-c
+	// can be refactored to push to another channel and sum that channel
+	fmt.Println(total)
+}
